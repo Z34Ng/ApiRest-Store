a8cd7f8bcbce0d3a9c6d37548c10d7bdd0955658
unknown <geminis2199@gmail.com>
2022-03-16 01:22:12 -0500

    Agregado servicios para seguridad

--- src/main/java/com/ecommerce/apireststore/security/service/IUsuarioService.java
+++ src/main/java/com/ecommerce/apireststore/security/service/IUsuarioService.java
@@ -1,13 +1,13 @@
- /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.ecommerce.apireststore.security.repository;
+package com.ecommerce.apireststore.security.service;
 
 /**
  *
  * @author ZEAN
  */
-public interface IUsuarioRepository {
+public interface IUsuarioService {
     
 }
