84a1642e5b939a10f662fd64fd47617f7dd1230d
unknown <geminis2199@gmail.com>
2022-03-16 23:50:57 -0500

    Agregado atributos y anotaciones de rol

--- src/main/java/com/ecommerce/apireststore/security/entity/Rol.java
+++ src/main/java/com/ecommerce/apireststore/security/entity/Rol.java
@@ -4,10 +4,32 @@
  */
 package com.ecommerce.apireststore.security.entity;
 
+import com.ecommerce.apireststore.security.enums.RolNombre;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
 /**
  *
  * @author ZEAN
  */
+@Entity
+@Data
+@Table(name="rol")
 public class Rol {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    
+    @NotNull
+    @Enumerated(EnumType.STRING) //se debe definir el tipo ya que por defecto se crea como int
+    private RolNombre rolNombre;
+   
     
 }
