9dd726302060af78f47031de9a7a4327a2466179
unknown <geminis2199@gmail.com>
2022-03-17 00:14:00 -0500

    Implementado clase para privilegios de cada usuario

--- src/main/java/com/ecommerce/apireststore/security/entity/UsuarioPrincipal.java
+++ src/main/java/com/ecommerce/apireststore/security/entity/UsuarioPrincipal.java
@@ -4,10 +4,84 @@
  */
 package com.ecommerce.apireststore.security.entity;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 /**
  *
  * @author ZEAN
  */
-public class UsuarioPrincipal {
+
+/*
+Implementa los privilegios de cada usuario
+*/
+public class UsuarioPrincipal implements UserDetails{
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    
+
+    //asigna los privilegios a c/usuario, la autorizacion
+    public static UsuarioPrincipal build(Usuario usuario){
+        List<GrantedAuthority> authorities = 
+                usuario.getRoles().stream().map(
+                    rol -> new SimpleGrantedAuthority(rol.getRolNombre().name()))
+                        .collect(Collectors.toList()); //convirtiendo clase rol a grantedauthority
+        
+        return new UsuarioPrincipal(usuario.getName(),usuario.getUsername(),usuario.getEmail(),usuario.getPassword(),authorities);
+    }
+    
+    public UsuarioPrincipal() {
+    }
+
+    public UsuarioPrincipal(String name, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
     
 }
