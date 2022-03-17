10c8e1681a4f45cc56327f905af9650eba1e5789
unknown <geminis2199@gmail.com>
2022-03-16 19:26:34 -0500

    Agregado clase para definición de roles

---
+++ src/main/java/com/ecommerce/apireststore/security/enums/RolNombre.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ecommerce.apireststore.security.enums;
+
+/**
+ *
+ * @author ZEAN
+ */
+
+/**
+ * Clase especial que predefine los únicos objetos
+ * 
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER;
+}
