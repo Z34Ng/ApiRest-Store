b8a22f8b21cbbc364e5076b0c166141a7ec7aac7
unknown <geminis2199@gmail.com>
2022-03-17 20:02:01 -0500

    Modificado ubicacion de UserDetailsServiceImpl

--- src/main/java/com/ecommerce/apireststore/security/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/apireststore/security/service/impl/UserDetailsServiceImpl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.ecommerce.apireststore.service.impl;
+package com.ecommerce.apireststore.security.service.impl;
 
 /**
  *
